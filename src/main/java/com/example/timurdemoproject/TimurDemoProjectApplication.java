93e9e71c4303ba3ab22eb31734d06e592435a796
KostiukStudent <kostiukvladyslav@gmail.com>
2019-02-13 18:48:44 +0100

    First commit

---
+++ src/main/java/com/example/timurdemoproject/TimurDemoProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.timurdemoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimurDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimurDemoProjectApplication.class, args);
+	}
+
+}
+
