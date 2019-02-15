4ac1859c840130dbab783977516bff9bd7e8fcc1
Тимур Абдулов <hostingurifa@gmail.com>
2019-02-15 20:02:02 +0500

    Update TimurDemoProjectApplication.java
    Тест

--- src/main/java/com/example/timurdemoproject/TimurDemoProjectApplication.java
+++ src/main/java/com/example/timurdemoproject/TimurDemoProjectApplication.java
@@ -8,7 +8,7 @@ public class TimurDemoProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TimurDemoProjectApplication.class, args);
-                // Test Record from Timur
+                
 	}
 
 }
